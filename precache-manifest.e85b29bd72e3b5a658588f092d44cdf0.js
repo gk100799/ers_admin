0897f5da0c0f8b1ceba2e7ce5f19123f98f2489f
gk100799 <55948183+gk100799@users.noreply.github.com>
2020-10-09 14:07:03 +0530

    Updates

---
+++ precache-manifest.e85b29bd72e3b5a658588f092d44cdf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed3f85cdc9e5cdf664372d55eee276b",
+    "url": "/ers_admin/index.html"
+  },
+  {
+    "revision": "19d8a7ab6cc362e07188",
+    "url": "/ers_admin/static/css/main.c67eaa3b.chunk.css"
+  },
+  {
+    "revision": "efaa6ae7f79ab797b24a",
+    "url": "/ers_admin/static/js/2.288a3a94.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ers_admin/static/js/2.288a3a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d8a7ab6cc362e07188",
+    "url": "/ers_admin/static/js/main.0b6409da.chunk.js"
+  },
+  {
+    "revision": "20b639e14803dbdb71a8",
+    "url": "/ers_admin/static/js/runtime-main.90f40fa6.js"
+  }
+]);
